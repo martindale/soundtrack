3ebaeb603b58f7f33a8c4f4ad6be3da83e0bd21d
Eric Martindale <eric@ericmartindale.com>
2020-11-25 10:38:08 -0500

    Streamline start/stop methods

--- types/stream.js
+++ types/stream.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const net = require('net');
+const Hash256 = require('@fabric/core/types/hash256');
 const Interface = require('@fabric/core/types/interface');
 
 class Stream extends Interface {
@@ -14,33 +15,57 @@ class Stream extends Interface {
     }, this.settings, settings);
 
     this.server = null;
+    this.sockets = {};
 
     return this;
   }
 
-  _handleConnection (socket) {
-    console.log('client connected');
+  async _handleConnection (socket) {
+    console.log('[SCRIPTS:STREAM]', '[client connected]', socket.remoteAddress);
+
+    let id = Hash256.digest(socket.remoteAddress);
+
+    if (this.sockets[id]) {
+      // TODO: notify client of disconnect
+      await this.sockets[id].destroy();
+    }
+
+    this.sockets[id] = socket;
+    // TODO: move to `_boundFunctions`
+    this.sockets[id]._fabricHandler = this.on('data', this._handleSocketData.bind(this));
 
     socket.on('end', () => {
-      console.log('client disconnected');
+      console.log('[SCRIPTS:STREAM]', '[client disconnected]', id);
     });
 
     // socket.write('Hello, world!');
   }
 
-  start () {
-    this.server = net.createServer(this._handleConnection.bind(this));
+  async _handleSocketData (data) {
+    console.log('[SCRIPTS:STREAM]', 'Socket data:', data);
+  }
 
-    this.server.on('error', (err) => {
+  async start () {
+    const self = this;
+
+    self.server = net.createServer(self._handleConnection.bind(self));
+
+    self.server.on('error', (err) => {
       throw err;
     });
 
-    this.server.listen(this.settings.port, () => {
-      console.log('server bound');
+    const promise = new Promise((resolve, reject) => {
+      self.server.listen(self.settings.port, (error) => {
+        if (error) return reject(error);
+        console.log('[SCRIPTS:STREAM]', '[server bound]', self.server.address);
+        resolve(self);
+      });
     });
+
+    return promise;
   }
 
-  stop () {
+  async stop () {
     if (this.server) this.server.close();
   }
 }
